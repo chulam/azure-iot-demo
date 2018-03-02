544b6fa9d0eed6529b41490e93ef3b35ba075e50
Stanley Lam <chulam@microsoft.com>
2018-03-02 14:23:10 +0800

    Initial commit

---
+++ aggregator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Aggregator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options => {
+                    options.UseConnectionLogging();
+                })
+                .UseUrls("http://+:5001")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
